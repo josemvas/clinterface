035abc15a6f3669e31613d3e5f1ace0725f5e0ea
jmvasquez <jmvasquez@helio.helio>
2019-12-23 22:51:10 -0600

    Improve setup configuration

--- setup.py
+++ setup.py
@@ -1,2 +1,8 @@
+python_requires = (3, 1)
+
+import sys
+if sys.version_info < python_requires:
+    sys.exit('Python {0} or higher is required to setup this package.'.format('.'.join(str(i) for i in python_requires)))
+
 import setuptools
 setuptools.setup()
